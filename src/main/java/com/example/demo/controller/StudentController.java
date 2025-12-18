c79dc8bd221678473e0708ed465ec51d25380ce0
bala-amypo <bala@amypo.in>
2025-12-18 05:19:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,30 +3,39 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
-
 @RestController
-public class StudentController{
-    @Autowired
-    StudentService src;
-    @PostMapping("/post")
-    public StudentEntity postdata(@RequestBody StudentEntity st){
-        return src.savedata(st);
+public class StudentController {
+@Autowired
+StudentService src;
+@PostMapping("/post")
 
+    public StudentEntity postdata(@RequestBody Studententity st){
+        return src.savedata(st);
     }
     @GetMapping("/get")
-    public List<StudentEntity> getdata(){
+    public List<StudentEntity>getdata(){
         return src.retdata();
-
     }
-    @GetMapping("/getid/{id}")
-    public  StudentEntity getIdVal(@PathVariable int id){
+    @GetMapping("/get/{id}")
+    public StudentEntity getIdval(@PathVariable int id){
         return src.id(id);
     }
+    @PutMapping("/updatedata/{id}")
+    public StudentEntity update(@PathVariable int id,@RequestBody StudentEntity st){
+        return src.updateStudent(id,st);
+    }
+    @DeleteMapping("/delete/{id}")
+    public StudentEntity deleteStudent(@PathVariable int id){
+        return src.deleteStudent(id);
+    }
+}
--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,60 +1,41 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
 
-@Entity
-public class StudentEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id;
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int rollno;
+
+public class StudentEntity {
+   
     private String name;
-    private LocalDate dob;
+    private int id;
     private String email;
-    private float cgpa;
-
-    public StudentEntity(String name, int id, LocalDate dob, String email, float cgpa) {
-        this.name = name;
-        this.id = id;
-        this.dob = dob;
-        this.email = email;
-        this.cgpa = cgpa;
-    }
-
-    public StudentEntity(){
-
-    }
     public String getName() {
         return name;
-    }
-    public void setName(String name) {
-        this.name = name;
     }
     public int getId() {
         return id;
     }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
     public String getEmail() {
         return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
     public void setEmail(String email) {
         this.email = email;
     }
-    public float getCgpa() {
-        return cgpa;
+    public StudentEntity(String name, int id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
+    public StudentEntity() {
     }
     
+
+    
+    
+    
+
 }
